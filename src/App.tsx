fd2b54084bce89599d1e681088146c8512775cbd
fff907 <59164817+fff907@users.noreply.github.com>
2026-01-06 16:24:25 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Thanks from './pages/Thanks.tsx';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/request" element={<Request />} />
