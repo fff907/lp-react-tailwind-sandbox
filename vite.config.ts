efd492857aa0af55a4667ae1770210fc8f428dd6
fff907 <59164817+fff907@users.noreply.github.com>
2026-01-06 16:50:39 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/vitejs-vite-mjq1gaza/' : '/',
+  base: command === 'build' ? '/lp-react-tailwind-sandbox/' : '/',
   plugins: [react()],
 }));
