0d44cc097d2c67764edd9407ee56e7b37836bc9b
fff907 <59164817+fff907@users.noreply.github.com>
2026-04-23 13:55:24 +0900

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
